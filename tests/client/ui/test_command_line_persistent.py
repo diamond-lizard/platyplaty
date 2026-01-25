7561e63347ab27e4d2e17c8ee34b5e83c4b7aec6
Sergey Goldgaber <large.chart5044@fastmail.com>
2026-01-24 19:56:04 -0500

    add CommandLine persistent message integration tests
    Add tests verifying CommandLine:
    - Yields PersistentMessage with correct ID
    - Has show_persistent_message method
    - Has clear_persistent_message method

--- tests/client/ui/test_command_line_persistent.py
+++ tests/client/ui/test_command_line_persistent.py
@@ -30,3 +30,23 @@ class TestCommandLineYieldsPersistentMessage:
         children = list(widget.compose())
         msg = next(c for c in children if isinstance(c, PersistentMessage))
         assert msg.id == "persistent_message"
+
+
+class TestCommandLineHasShowPersistentMethod:
+    """Tests that CommandLine has show_persistent_message method."""
+
+    def test_has_show_persistent_message_method(self) -> None:
+        """CommandLine has show_persistent_message method."""
+        widget = CommandLine()
+        assert hasattr(widget, "show_persistent_message")
+        assert callable(widget.show_persistent_message)
+
+
+class TestCommandLineHasClearPersistentMethod:
+    """Tests that CommandLine has clear_persistent_message method."""
+
+    def test_has_clear_persistent_message_method(self) -> None:
+        """CommandLine has clear_persistent_message method."""
+        widget = CommandLine()
+        assert hasattr(widget, "clear_persistent_message")
+        assert callable(widget.clear_persistent_message)
